18689b1e74cc1251104acad8eebf8db813ff70d7
zhi.zhong <zhi.zhong@qunar.com>
2014-12-09 11:12:52 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var join = require('path').join;
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+module.exports = yeoman.generators.Base.extend({
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+    this.pkg = require('../package.json');
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // welcome message
+    if (!this.options['skip-welcome-message']) {
+      this.log(require('yosay')());
+      this.log(chalk.magenta(
+        'Out of the box I include Velocity template parser, and a ' +
+        'Gruntfile.js to build your app.'
+      ));
+    }
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'mavenRelate',
+        value: 'includeMavenRelate',
+        checked: false
+      }]
+    }];
+
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.includeMavenRelate = hasFeature('includeMavenRelate');
+      done();
+    }.bind(this));
+  },
+
+  gruntfile: function () {
+    this.template('Gruntfile.js');
+  },
+
+  packageJSON: function () {
+    this.template('_package.json', 'package.json');
+  },
+
+  git: function () {
+    this.template('gitignore', '.gitignore');
+  },
+
+  pomXML: function() {
+    if (this.includeMavenRelate) {
+      this.template('pom.xml');
+    }
+  },
+
+  readme: function() {
+    this.template('README.md');
+  },
+
+  app: function () {
+    this.directory('app');
+    this.mkdir('app/static/js');
+  },
+
+  config: function () {
+    this.directory('config');
+  }
+
+});
